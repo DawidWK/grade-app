61165057b52466943b0ea854dcb4444950fa2660
Ynfus <bartekzakrz2001@gmail.com>
2023-12-10 11:06:39 +0100

    add permission to register page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'permission'=> $request->permission,
         ]);
 
         event(new Registered($user));
