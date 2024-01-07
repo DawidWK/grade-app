e294582c1ba25d23f175a815003747bceeaf9fff
Ynfus <bartekzakrz2001@gmail.com>
2024-01-07 16:02:52 +0100

    update grade delete

--- resources/views/grades/edit.blade.php
+++ resources/views/grades/edit.blade.php
@@ -41,6 +41,13 @@
                             <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm mt-1 block w-full" name="description" placeholder="Opis oceny">{{ $grade->description }}</textarea>
                         </div>
                         <button class="mt-6 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" type="submit">Zapisz</button>
+                        <form action="{{ route('grades.destroy', $grade->id) }}" method="POST" class="mt-4">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                Usuń ocenę
+                            </button>
+                        </form>
                     </form>
                 </div>
             </div>
